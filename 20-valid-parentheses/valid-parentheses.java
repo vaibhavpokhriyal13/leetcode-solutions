ebbe6c002c1a2eb235f32acb505f1b2756d9ecf4
vaibhavpokhriyal13 <vaibhavpokhriyal13@gmail.com>
2026-06-30 15:17:29 +0530

    Time: 4 ms (38.44%) | Memory: 43.3 MB (40.69%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack <Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
+                st.push(s.charAt(i));
+            } else {
+                if (st.empty()) {
+                    return false;
+                }
+                 char top=st.pop();
+
+        if(s.charAt(i)==')' && top!= '('){
+            return false;
+        }
+        if (s.charAt(i)== '}' && top != '{') {
+                    return false;
+                }
+
+                if (s.charAt(i)== ']' && top != '[') {
+                    return false;
+                }
+        }
+        }
+       return st.isEmpty();
+    
+        
+    }
+    
+}
