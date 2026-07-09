8c5c792dddb7024f8cf13a71dc78671826e12faf
vaibhavpokhriyal13 <vaibhavpokhriyal13@gmail.com>
2026-07-09 09:36:29 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (95.57%) - LeetSync

---
+++ 46-permutations/permutations.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> arr= new ArrayList<>();
+        getperm(nums,arr,0);
+        return arr;
+    }
+
+    public void getperm(int[] nums,List<List<Integer>> arr,int i){
+        if(i==nums.length){
+            List<Integer> temp = new ArrayList<>();
+            for (int j=0;j<nums.length;j++) {
+                temp.add(nums[j]);
+            }
+            arr.add(temp);
+            return;
+        }
+        for(int k=i; k < nums.length; k++){
+            swap(nums,i,k);
+            getperm(nums,arr,i+1);
+            swap(nums,i,k);
+        }
+    }
+        public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+}
