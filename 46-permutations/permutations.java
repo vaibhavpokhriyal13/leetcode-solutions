dd910b885b39cb5eb5be39b9f065cecd6dcfeec7
vaibhavpokhriyal13 <vaibhavpokhriyal13@gmail.com>
2026-07-17 09:57:14 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (63.62%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,29 +1,33 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> arr= new ArrayList<>();
-        getperm(nums,arr,0);
-        return arr;
+        List<List<Integer>> arr=new ArrayList<>();
+        getParam(nums,arr,0);
+        return arr; 
     }
 
-    public void getperm(int[] nums,List<List<Integer>> arr,int i){
+    public void getParam(int[] nums,List<List<Integer>> arr,int i){
         if(i==nums.length){
-            List<Integer> temp = new ArrayList<>();
-            for (int j=0;j<nums.length;j++) {
+            List<Integer> temp=new ArrayList<>();
+            for(int j=0;j<nums.length;j++){
                 temp.add(nums[j]);
             }
             arr.add(temp);
             return;
+
         }
-        for(int k=i; k < nums.length; k++){
+        for(int k=i;k<nums.length;k++){
             swap(nums,i,k);
-            getperm(nums,arr,i+1);
+            getParam(nums,arr,i+1);
             swap(nums,i,k);
         }
     }
-        public void swap(int[] nums, int i, int j) {
+
+
+
+
+      public void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
-
 }
